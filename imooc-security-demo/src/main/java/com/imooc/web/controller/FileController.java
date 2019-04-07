9275ef61de8815b99af288d322f6cdcf1dd27f07
yongxu007 <yongxu16@163.com>
2019-04-08 00:15:00 +0800

    modify pom file

--- imooc-security-demo/src/main/java/com/imooc/web/controller/FileController.java
+++ imooc-security-demo/src/main/java/com/imooc/web/controller/FileController.java
@@ -3,24 +3,18 @@
  */
 package com.imooc.web.controller;
 
+import com.imooc.dto.FileInfo;
+import org.apache.commons.io.IOUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.io.IOUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.imooc.dto.FileInfo;
 
 /**
  * @author zhailiang
@@ -39,7 +33,7 @@ public class FileController {
 		System.out.println(file.getOriginalFilename());
 		System.out.println(file.getSize());
 
-		File localFile = new File(folder, new Date().getTime() + ".txt");
+		File localFile = new File(folder, System.currentTimeMillis() + ".txt");
 
 		file.transferTo(localFile);
 
